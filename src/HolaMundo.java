5edc600c219fcc2426174ed899c164c6a57f957b
Jorge Martínez <jmmostazo.medac@gmail.com>
2022-07-25 11:05:16 +0200

    Commit cuatro Mi nombre

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 		String nombre = "Jorge";
+		System.out.println("Mi nombre es : "+nombre);
 	}
 
 }
