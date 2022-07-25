5eaf64344122fb061edf9cc21642bcd227a58f81
Jorge Martínez <jmmostazo.medac@gmail.com>
2022-07-25 12:07:54 +0200

    commit cinco mi edad

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -6,6 +6,7 @@ public class HolaMundo {
 		System.out.println("Hola Mundo");
 		String nombre = "Jorge";
 		System.out.println("Mi nombre es : "+nombre);
+		System.out.println("Mi edad es 49 años");
 	}
 
 }
