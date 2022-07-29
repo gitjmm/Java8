d2c5bd0babfa8f910c94967dc980633b7f0483aa
Jorge Martínez <jmmostazo.medac@gmail.com>
2022-07-29 10:58:46 +0200

    Final hundir la flota

--- src/hundirFlota/HundirFlota.java
+++ src/hundirFlota/HundirFlota.java
@@ -131,6 +131,118 @@ public class HundirFlota {
 		
 	}
 	
+	/*
+	 * Método que imprime un mapa, con una fila y columna de encabezados
+	 */
+	
+	private static void imprimirMapa(char[][] mapa) {
+		// Calculamos las letras
+		char[] letras = new char[TAMANIO];
+		for (int i=0; i<TAMANIO; i++)
+			letras[i] = (char)('A'+i);
+		
+		// Imprimir fila de encabezado
+		System.out.println("   ");
+		System.out.print("    ");
+		for (int i = 0; i<TAMANIO; i++){
+			System.out.print("["+ i + "]");
+			System.out.print(" ");
+		}
+		
+		System.out.println("");
+		
+		// Imprimimos resto de filas
+		for (int i = 0; i < TAMANIO; i++) {
+			System.out.print("[" + letras[i] + "]  ");
+			for (int j = 0; j < TAMANIO; j++) {
+				System.out.print(mapa[i][j] + "   ");
+			}
+			System.out.println("");
+		}
+		
+	}
+	
+	// Método por el que el usuario introduce una casilla de tirada
+	private static int[] pedirCasilla() {
+		System.out.println("Introduzca casilla (Ejemplo A1)");
+		String linea = sc.nextLine();
+		linea = linea.toUpperCase();
+		int[] t;
+		
+		// Comprobamos que lo introducido por el usuario es correcto con expresiones regulares
+		if (linea.matches("^[A-Z][0-9]*$")) {
+			// Obtenemos letra
+			char letra = linea.charAt(0);
+			// getNumericValue - Devuelve el valor entero de un caracter. 
+			// El número de fila es VALOR_NUMERICO(LETRA) - VALOR_NUMERICO(A).
+			int fila = Character.getNumericValue(letra) - Character.getNumericValue('A');
+			// Para la columna, tan solo tenemos que procesar el número
+			int columna = Integer.parseInt(linea.substring(1, linea.length()));
+			// Si las coordenadas están dentro del tamaño del tablero, las devolvemos
+			if (fila >= 0 && fila < TAMANIO && columna >= 0 && columna <= TAMANIO) {
+				t = new int[]{ fila, columna };
+			} else //En otro caso, devolvemos -1, para que vuelva a solicitar el tiro
+				t =  new int[] { -1, -1 };
+		} else 
+			t =  new int[] { -1, -1 };
+		
+		
+		return t;
+		}
+	
+	/*
+	 * Método que nos permite evaluar si un tiro es CORRECTO (AGUA o TOCADO)
+	 * o se trata de una casilla por la que ya hemos pasado antes.
+	 */
+	
+	
+	public static boolean evaluarTiro(char[][] mapa, int[] t) {
+		int fila = t[0];
+		int columna = t[1];
+		return mapa[fila][columna] == AGUA_NO_TOCADO || (mapa[fila][columna] >= '1' && mapa[fila][columna] <= '5'); 
+		
+	}
+	
+	/*
+	 * Método que actualiza el mapa, con un determinado tiro. 
+	 * Devolvemos el número de puntos restantes.
+	 */
+	private static int actualizarMapa(char[][] mapa, int[] t, int puntos) {
+		int fila = t[0];
+		int columna = t[1];
+		
+		if (mapa[fila][columna] == AGUA_NO_TOCADO) {
+			mapa[fila][columna] = AGUA;
+			System.out.println("AGUA");
+		} else {			
+			mapa[fila][columna] = TOCADO;
+			System.out.println("HAS ALCANZADO A ALGÚN BARCO");
+			--puntos;
+		}
+		
+		return puntos;
+		
+		
+	}
+	
+	/*
+	 * Método que sirve para que el ordenador pueda hacer un disparo
+	 */
+	private static int[] generaDisparoAleatorio() {		
+		return new int[] {aleatorio(), aleatorio()};
+	}
+	
+	/*
+	 * Método que actualiza el mapa de registro
+	 */
+	private static void actualizarMapaRegistro(char[][] mapa, int[] t, char valor) {
+		int fila = t[0];
+		int columna = t[1];
+
+		mapa[fila][columna] = valor;
+	}
+
+	
 	
 	
 	/* --------MAIN--------------*/
@@ -166,9 +278,76 @@ public class HundirFlota {
 		//Mapa de tiradas. Inicializamos a AGUA_NO_TOCADO
 		inicializaMapaRegistro(mapaOrdenadorParaUsuario);
 		
-		
+		while (!juegoTerminado) {
+			
+			//Mostramos mapa usuario
+			System.out.println("Mapa Usuario");
+			imprimirMapa(mapaUsuario);
+			
+			System.out.printf("PUNTOS RESTANTES DEL JUGADOR: %d\n",puntosUsuario);
+			System.out.println("TURNO DEL JUGADOR");
+			
+			// Tirada de usuario
+			tiroCorrecto = false;
+			while (!tiroCorrecto) {
+				// Solicitamos datos del tiro por teclado
+				tiro = pedirCasilla();
+				
+				// Verificar si el tiro es correcto
+				if (tiro[0] != -1 && tiro[1] != -1) {
+					tiroCorrecto = evaluarTiro(mapaOrdenador,tiro);
+					if (!tiroCorrecto) 
+						System.out.println("TIRO INCORRECTO");
+				}else {
+					System.out.println("TIRO INCORRECTO");
+				}
+				
+			}
+			
+			//Actualizamos mapa de ordenador y puntos
+			int puntosOrdenadorAnterior = puntosOrdenador;
+			puntosOrdenador = actualizarMapa(mapaOrdenador, tiro, puntosOrdenador);
+			
+			// Si el número de puntos es > 0 es tocado sino es agua
+			char tipoTiro = (puntosOrdenadorAnterior - puntosOrdenador) > 0 ? TOCADO : AGUA; 
+			actualizarMapaRegistro(mapaOrdenadorParaUsuario,tiro, tipoTiro);
+			System.out.println("\nREGISTRO DEL MAPA DEL ORDENADOR");
+			imprimirMapa(mapaOrdenadorParaUsuario);
+			
+			//El juego termina si el número de puntos llega a 0
+			juegoTerminado = (puntosOrdenador == 0);
+			
+			
+			//Si no ha ganado el jugador, le toca a la máquina
+			if (!juegoTerminado) {
+				
+				System.out.printf("PUNTOS RESTANTES DEL ORDENADOR: %d\n\n", puntosOrdenador);
+				System.out.println("TURNO DEL ORDENADOR");
+				tiroCorrecto = false;
+				//Seguimos los mismos parámetros de comprobación que en la tirada del usuario
+				while (!tiroCorrecto) {
+					tiro = generaDisparoAleatorio();
+					tiroCorrecto = evaluarTiro(mapaUsuario, tiro);
+				}
+			}
+			//Actualizamos mapa
+			puntosUsuario = actualizarMapa(mapaUsuario, tiro, puntosUsuario);
+			//El juego termina si el número de puntos llega a 0			
+			juegoTerminado = (puntosUsuario == 0);
+
+			} //FIN DE LA PARTIDA. Alguien ha ganado
+					
+			if (puntosOrdenador == 0) {
+				System.out.println("EL VENCEDOR HA SIDO EL JUGADOR");
+			} else 
+				System.out.println("EL VENCEDOR HA SIDO EL ORDENADOR");
+	
+			sc.close();
+			
+			
+		}
 		
 		
 	}
 
-}
+
